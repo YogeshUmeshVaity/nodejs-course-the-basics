ee3e62f1cde2435e4e48be91453cdf28c8ba0e16
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2020-10-06 15:58:29 +0530

    Send registration form on home page and parse the user name specified by the user in the server console.

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,32 @@ import http, { IncomingMessage, ServerResponse } from 'http'
 
 const handler = (request: IncomingMessage, response: ServerResponse) => {
   const url = request.url
+  const method = request.method
   if (url === '/') {
-    sendWelcomeMessage(response)
+    sendRegistrationForm(response)
   }
   if (url === '/users') {
     sendUserList(response)
+  }
+  if (url === '/create-user' && method === 'POST') {
+    parseUserName(request)
   }
   response.end()
 }
 
 http.createServer(handler).listen(3000, () => console.log('started'))
+
+function parseUserName(request: http.IncomingMessage) {
+  const body: Uint8Array[] = []
+  request.on('data', (chunk) => {
+    body.push(chunk)
+  })
+  request.on('end', () => {
+    const userName = Buffer.concat(body).toString()
+    console.log(`User name to be created: ${userName}`)
+  })
+}
+
 function sendUserList(response: http.ServerResponse) {
   response.writeHead(200, { 'Content-Type': 'text/html' })
   response.write('<html')
@@ -26,9 +42,19 @@ function sendUserList(response: http.ServerResponse) {
   response.write('</ul>')
   response.write('</body>')
   response.write('</html>')
-  response.end
+  response.end()
 }
 
-function sendWelcomeMessage(response: http.ServerResponse) {
-  response.end('Welcome to the Node.js Basics')
+function sendRegistrationForm(response: http.ServerResponse) {
+  response.writeHead(200, { 'Content-Type': 'text/html' })
+  response.write('<html')
+  response.write('<head><title>Register</title></head>')
+  response.write('<body>')
+  response.write('<h1> Create a new user </h1>')
+  response.write('<form action="/create-user" method="POST">')
+  response.write('<input type="text" name="userName"> <button type="submit">Send</button>')
+  response.write('</form>')
+  response.write('</body>')
+  response.write('</html>')
+  response.end()
 }
