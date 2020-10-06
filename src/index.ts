8a6e9a2463f0dcb943886609f7476f26f81e9f0f
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2020-10-06 15:15:41 +0530

    Display list of users for the '/users' endpoint.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,34 @@
 import http, { IncomingMessage, ServerResponse } from 'http'
 
 const handler = (request: IncomingMessage, response: ServerResponse) => {
-  http
-    .request('http://jsonplaceholder.typicode.com/posts/1', function (externalResponse) {
-      externalResponse.pipe(response)
-    })
-    .on('error', function (e) {
-      response.statusCode = 500
-    })
-    .end()
+  const url = request.url
+  if (url === '/') {
+    sendWelcomeMessage(response)
+  }
+  if (url === '/users') {
+    sendUserList(response)
+  }
+  response.end()
 }
 
-http.createServer(handler).listen(8080, () => console.log('started'))
+http.createServer(handler).listen(3000, () => console.log('started'))
+function sendUserList(response: http.ServerResponse) {
+  response.writeHead(200, { 'Content-Type': 'text/html' })
+  response.write('<html')
+  response.write('<head><title>User List</title></head>')
+  response.write('<body>')
+  response.write('<h1> List of Users </h1>')
+  response.write('<ul>')
+  response.write('<li> Sandy </li>')
+  response.write('<li> Vighu </li>')
+  response.write('<li> Jane </li>')
+  response.write('<li> Komaru </li>')
+  response.write('</ul>')
+  response.write('</body>')
+  response.write('</html>')
+  response.end
+}
+
+function sendWelcomeMessage(response: http.ServerResponse) {
+  response.end('Welcome to the Node.js Basics')
+}
